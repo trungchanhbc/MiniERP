ea9165c6b640add7d789c2cafa356777b3f0ef72
Chanh, Nguyen Trung <trungchanhbc@hotmail.com>
2020-01-31 03:05:50 +0700

    Add MaterialDesignTheme

---
+++ Model/Model.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniERP.Model
+{
+    public class User
+    {
+        private string _username;
+        public string UserName
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+    }
+}
